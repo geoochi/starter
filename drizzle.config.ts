b6ba1c7a837c1dc18638b8c2b44b9dfb8d08f510
Geoo Chi <geoochi@geoochi.com>
2025-09-19 15:11:46 +0800

    format: use prettier

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "drizzle-kit";
-import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit'
+import { config } from 'dotenv'
 
-process.env.NODE_ENV !== 'production' ? config({ path: '.env' }) : config({ path: '.env.local' })// or .env.local
+process.env.NODE_ENV !== 'production' ? config({ path: '.env' }) : config({ path: '.env.local' }) // or .env.local
 config({ path: '.env.local' })
 export default defineConfig({
-    schema: "./utils/db/schema.ts",
-    out: "./utils/db/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
-});
+  schema: './utils/db/schema.ts',
+  out: './utils/db/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+})
